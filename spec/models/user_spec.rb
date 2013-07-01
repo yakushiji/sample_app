bd0c311d5c885e8b46f154a2cf1b3bf33dd21f62
yakushiji <yaoshisizhenghe@gmail.com>
2013-07-02 02:43:56 +0900

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,10 +26,21 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token)}
+
+  it { should respond_to(:admin)}
   it { should respond_to(:authenticate) }
 
-  
   it { should be_valid}
+  it { should_not be_admin}
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when email format is invalid" do
     it "should be invalid" do
